f381e5870f3fd4148cf69c7b53387aa7ec216ff2
youssifsayed <sayedgyussif@gmail.com>
2014-10-15 21:17:20 +0200

    change mysql to mysqli

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
-$condb = mysql_connect("localhost","root","") or die ("not connect");
-$seldb = mysql_select_db("blog",$condb) or die ("nooo icant con");
+$condb = new mysqli("localhost","root","") or die ("not connect");
+$seldb = $condb->select_db("blog",$condb) or die ("nooo icant con");
 
-mysql_query("set character_set_server='utf8'");
-mysql_query("set names 'utf8'");
+$condb-query("set character_set_server='utf8'");
+$condb-query("set names 'utf8'");
 ?>
