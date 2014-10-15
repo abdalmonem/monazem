e9584766769608d8d7e99bc88bfca4fcd365c92e
youssifsayed <sayedgyussif@gmail.com>
2014-10-15 21:27:17 +0200

    Create user.lib.php

---
+++ classes/user.lib.php
@@ -0,0 +1,22 @@
+<?php
+  /**
+    @class : user 
+    @date : 9:32pm 
+    @author : youssifsayed
+    @using : get user from datebase
+  */
+  class user{
+    
+    /**
+      * string mksql() *
+      
+      @prame array $a 
+      @prame string $type : sql type [where,delete,update]
+      @using : change array to sql string  
+      @date : 9:32pm 
+    */
+    public function mksql($q ,$type){
+      
+    }
+  }
+?>
